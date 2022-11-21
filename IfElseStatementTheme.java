52c3a433314cad26e97eb4f2119b6339de410f7a
Sergey <nagoymail@gmail.com>
2022-11-21 12:50:21 +0500

    исправление ошибок

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -1,5 +1,5 @@
 public class IfElseStatementTheme {
-    public static void main(String[] args) {        
+    public static void main(String[] args) {
         System.out.println("№-1 Перевод псевдокода на язык Java");
         int age = 18;
         boolean isMale = true;
