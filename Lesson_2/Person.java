169836a31d8f88baa0f0d5c602185eb1d30cdfa1
IvannikovSergey <Nnagoy@yandex.com>
2022-12-05 18:26:41 +0500

    #

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String sex = "male";
+    String name = "Ivan";
+    int height = 185;
+    int weight = 90;
+    int age = 30;
+
+    public void go() {
+        System.out.println("Я могу ходить");
+    }
+
+    public void sit() {
+        System.out.println("Я могу сидеть");
+    }
+
+    public void run() {
+        System.out.println("Я могу бежать");
+    }
+
+    public void say() {
+        System.out.println("Я могу говорить");
+    }
+
+    public void learnJava() {
+        System.out.println("Я учу Java");
+    }
+}
